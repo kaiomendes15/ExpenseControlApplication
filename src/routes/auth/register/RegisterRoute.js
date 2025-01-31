6da99552868b75dd9b812095a87970fe615d97cd
Kaio <kakacnmendes@gmail.com>
2025-01-31 09:44:11 -0300

    Rota para excluir usuários (colocar depois em uma pasta destinada apenas para userServices)

--- src/routes/auth/register/RegisterRoute.js
+++ src/routes/auth/register/RegisterRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // CONTROLLER
 const registerController = require('../../../controllers/authController/RegisterController')
+const UserServiceController = require('../../../controllers/userServiceController')
 router.post('/auth/register', registerController.registerUser);
+router.delete('/auth/register/:username', UserServiceController.deleteUser);
 
 
 module.exports = router
