7091f0ff37ee0767fe4e0f42e572545503be3ad9
Kaio <kakacnmendes@gmail.com>
2025-02-03 14:44:24 -0300

    Verificações do controlador de serviços do usuário

--- src/controllers/userServiceController.js
+++ src/controllers/userServiceController.js
@@ -1,20 +1,20 @@
-const UserService = require('../models/services/userServices')
+const UserService = require('../models/services/userServicesModel')
 
 exports.deleteUser = ((req, res) => {
     console.log("Deletando usuário")
-    console.log("Corpo da requisição:", req.body); // Adicionando para depuração
+    // console.log("Corpo da requisição:", req.body); // Adicionando para depuração
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ message: 'Todos os campos são obrigatórios.' })
+        return res.status(400).json({ message: 'All fields are required.' })
     }
 
     const service = new UserService()
     const result = service.deleteUser(email, password)
 
     if(!result) {
-        return res.status(400).json({message: "Email não cadastrado e/ou senha inválida."})
+        return res.status(400).json({message: "invalid data"})
     }
 
-    return res.status(200).json({message: "Usuário deletado com sucesso."})
+    return res.status(200).json({message: "User deleted successfully."})
 })
