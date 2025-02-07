cd31d35334f7210aa1ff114aea3947ab79fe9d88
Kaio <kakacnmendes@gmail.com>
2025-02-07 18:57:24 -0300

    refatoração nas mensagens de erro

--- src/controllers/userServiceController.js
+++ src/controllers/userServiceController.js
@@ -1,19 +1,33 @@
 const UserService = require('../models/services/userServicesModel')
 
-exports.deleteUser = ((req, res) => {
+exports.deleteUser = (async (req, res) => {
     console.log("Deletando usuário")
     // console.log("Corpo da requisição:", req.body); // Adicionando para depuração
-    const { email, password } = req.body;
+    const { email, password, verifyPassword } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({ message: 'All fields are required.' })
+    if (!email) {
+        return res.status(400).json({ message: 'Email is required' })
+    }
+    if (!password) {
+        return res.status(400).json({ message: 'Password is required.' })
+    }
+    if (!verifyPassword) {
+        return res.status(400).json({ message: 'Please verify your password' })
+    }
+
+    if (password !== verifyPassword) {
+        return res.status(400).json({ message: 'Passwords do not match' })
     }
 
     const service = new UserService()
-    const result = service.deleteUser(email, password)
+    const result = await service.deleteUser(email, password)
 
-    if(!result) {
-        return res.status(400).json({message: "invalid data"})
+    if(result === 'User not found.') {
+        return res.status(404).json({message: 'User not found.'})
+    }
+    if (result === "incorrect password.") {
+        return res.status(401).json({message: "incorrect password."})
+
     }
 
     return res.status(200).json({message: "User deleted successfully."})
