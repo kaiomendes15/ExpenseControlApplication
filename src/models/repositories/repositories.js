765b230bfed0cec64f1c08dd356859a113997063
Kaio <kakacnmendes@gmail.com>
2025-02-07 18:58:14 -0300

    add verifyJWT e getUserInfos

--- src/models/repositories/repositories.js
+++ src/models/repositories/repositories.js
@@ -1,5 +1,8 @@
 const pool = require('../data/')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
 
 // verifica se o email está no banco de dados
 async function verifyUser(email) {
@@ -10,8 +13,16 @@ async function verifyUser(email) {
 
 };
 
+// pega todas as informações do usuário
+async function getUserInfos(userId) {
+    const query = 'SELECT username, email FROM users WHERE id = $1'
+    const result = await pool.query(query, [userId])
+    // console.log(result.rows[0])
+    return result.rows[0]
+}
+
 // verifica a senha no banco de dados
-async function verifyPassowrd(email, password) {
+async function verifyPassword(email, password) {
     const hashedPassword = await pool.query('SELECT password FROM users WHERE email = $1', [email])
     
         const passwordMatch = await bcrypt.compare(password, hashedPassword.rows[0].password)
@@ -26,5 +37,17 @@ async function getUserId(email) {
     return result.rows[0].id
 }
 
+async function verifyJWT(req, res, next) {
+    const token = req.headers['x-access-token'];
 
-module.exports = { verifyUser, verifyPassowrd, getUserId }
+    jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+        if (err) return res.status(401).json({message: 'Unauthorized user'});
+
+        req.userId = decodedToken.userId;
+        console.log(`userId: ${req.userId}`)
+        next()
+    })
+}
+
+
+module.exports = { verifyUser, verifyPassword, getUserId, verifyJWT, getUserInfos }
