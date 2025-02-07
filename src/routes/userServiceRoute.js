ec0416d7e08efc1906b8e17f3b4cd45af20cc323
Kaio <kakacnmendes@gmail.com>
2025-02-07 19:01:15 -0300

    adicionando a verificação jwt nas rotas

--- src/routes/userServiceRoute.js
+++ src/routes/userServiceRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { verifyJWT } = require('../models/repositories/repositories')
 
 const UserServiceController = require('../controllers/userServiceController')
 
-router.delete('/config/delete/', UserServiceController.deleteUser);
+router.delete('/config/delete/', verifyJWT, UserServiceController.deleteUser);
 
 
 module.exports = router
