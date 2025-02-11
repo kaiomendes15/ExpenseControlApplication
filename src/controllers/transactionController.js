c133bdd54991ce222f5ca50b409d0bdb8aa32341
Kaio <kakacnmendes@gmail.com>
2025-02-11 11:17:17 -0300

    visualizar e editar transações

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -6,23 +6,41 @@ exports.createTransaction = (async (req, res, next) => {
     const transaction = await service.createTransaction(req.userId, amount, category, type, note)
 
     if (transaction === 'User not found.') {
-        return res.status(401).json({message: 'User not found.'})
+        return res.status(404).json({message: 'User not found.'})
     }
 
-    console.log(transaction)
+    // console.log(transaction)
 
 })
 
 exports.getTransactionByUserId = (async (req, res, next) => {
-    console.log(`pegando as transaÇões do user de id = ${req.userId}`)
+    // console.log(`pegando as transaÇões do user de id = ${req.userId}`)
     const service = new TransactionModel();
     const transactions = await service.getUserTransactions(req.userId)
 
-    if (transactions === 'User not found.') {
-        return res.status(401).json({message: 'User not found.'})
+    // console.log(transactions)
+    return res.status(200).json({transactions})
+
+})
+
+exports.getTransactionById = (async (req, res, next) => {
+    const id = req.params.id
+    const service = new TransactionModel();
+    const transaction = await service.getTransactionById(id)
+
+    if (transaction === `Transaction not found.`) {
+        return res.status(404).json({message : `Transaction not found.`})
     }
 
-    console.log(transactions)
-    return res.json({transactions})
+    return res.status(200).json({transaction})
+})
 
+exports.editTransactionById = (async (req, res, next) => {
+    const id = req.params.id;
+    const { amount, category, type, note } = req.body;
+
+    const service = new TransactionModel();
+    const update = await service.editTransactionById(id, amount, category, type, note)
+
+    return res.json({update})
 })
--- src/models/services/transactionModel.js
+++ src/models/services/transactionModel.js
@@ -5,7 +5,7 @@ const { getUserInfos, verifyUser } = require('../repositories/repositories')
 module.exports = class Transactions {
     constructor(){}
     async createTransaction(userId, amount, category, type, note = '') {
-        console.log("criação de transação")
+        // console.log("criação de transação")
         const user = getUserInfos(userId)
         const userExist = verifyUser(user.email)
         if (!userExist) {
@@ -24,5 +24,48 @@ module.exports = class Transactions {
         const tables = await pool.query(query, [userId]);
 
         return tables.rows
+    }
+
+    async getTransactionById(transactionId) {
+        try {
+            const query = 'SELECT * FROM transactions WHERE id = $1';
+            const table = await pool.query(query, [transactionId]);
+            
+            if (table.rows.length === 0) {
+                return `Transaction not found.`
+            }
+
+            return table.rows
+
+        } catch(err) {
+            console.log(err)
+            return `ERRO: ${err}`
+
+        }
+
+
+        
+    }
+
+    async editTransactionById(transactionId, amount, category, type, note = '') {
+        const query = `
+  BEGIN;
+  UPDATE transactions 
+  SET amount = $1, category = $2, type = $3, note = $4 
+  WHERE id = $5;
+  ROLLBACK;
+  COMMIT;
+`;
+        const update = await pool.query(query, [transactionId, amount, category, type, note]);
+
+        console.log(update)
+        return update
+    }
+
+    async deleteTransactionById(transactionId) {
+        const query = 'DELETE FROM transactions WHERE id = $1';
+        const result = await pool.query(query, [transactionId])
+
+        return result
     }
 }
