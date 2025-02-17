59b3749e8ded935be0bad988bc394923ceac098f
Kaio <kakacnmendes@gmail.com>
2025-02-17 14:46:35 -0300

    Rota de resumo completo

--- src/routes/reportsRoute.js
+++ src/routes/reportsRoute.js
@@ -7,5 +7,6 @@ const reportsController = require('../controllers/reportsController')
 
 router.get('/reports/summary/income', verifyJWT, reportsController.Income)
 router.get('/reports/summary/expenses', verifyJWT, reportsController.Expenses)
+router.get('/reports/summary/:category', verifyJWT, reportsController.Summary)
 
 module.exports = router
