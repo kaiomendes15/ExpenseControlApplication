34f77af79399143d9ef2bd63048adb55d4a135f5
Kaio <kakacnmendes@gmail.com>
2025-02-18 14:40:11 -0300

    Rota para o calculo de saldos

--- src/routes/reportsRoute.js
+++ src/routes/reportsRoute.js
@@ -8,5 +8,6 @@ const reportsController = require('../controllers/reportsController')
 router.get('/reports/summary/income', verifyJWT, reportsController.Income)
 router.get('/reports/summary/expenses', verifyJWT, reportsController.Expenses)
 router.get('/reports/summary/:category', verifyJWT, reportsController.Summary)
+router.get('/reports/balance', verifyJWT, reportsController.Balance)
 
 module.exports = router
