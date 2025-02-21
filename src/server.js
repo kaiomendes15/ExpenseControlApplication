7f8e11cd3e136463771f14f4223d8b446763d23c
Kaio <kakacnmendes@gmail.com>
2025-02-21 18:15:40 -0300

    Mudando para banco remoto

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express()
 const PORT = 3000;
+const { neon } = require("@neondatabase/serverless");
+require('dotenv').config();
+const http = require("http");
+// const sql = neon(process.env.DATABASE_URL);
+const pool = require('./models/data/index')
 
 // rotas 
 const registerRouter = require('./routes/auth/register/RegisterRoute');
@@ -8,6 +13,7 @@ const loginRouter = require('./routes/auth/login/LoginRoute');
 const userServiceRouter = require('./routes/userServiceRoute');
 const transactionRouter = require('./routes/transactions')
 const reportsRouter = require('./routes/reportsRoute')
+
 
 // middleware para processar o corpo das requisições
 app.use(express.json())
@@ -21,6 +27,29 @@ app.use(transactionRouter)
 app.use(reportsRouter)
 
 
+const requestHandler = async (req, res) => {
+    const result = await pool.query(`SELECT version()`);
+    const { version } = result[0];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(version);
+  };
+
+  app.get('/db-version-pool', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT version()`);
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: "Erro ao conectar ao banco com Pool", details: err.message });
+    }
+});
+
+//   http.createServer(requestHandler).listen(3000, () => {
+//     console.log("Server running at http://localhost:3000");
 app.listen(PORT, () => {
-    console.log(`Server rodando na porta ${PORT}`)
-})
+        console.log(`Server rodando na porta ${PORT}`)
+    })
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server rodando na porta ${PORT}`)
+// })
